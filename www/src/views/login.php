68fc467e9badc4c34eef636073bf3a7a72d0f8b9
VashRogers <rogerssj2@gmail.com>
2022-10-04 11:28:49 -0400

    Finalizando tela de login

--- www/src/views/login.php
+++ www/src/views/login.php
@@ -5,15 +5,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <!-- <link rel="stylesheet" href="/assets/css/bulma.min.css"> -->
-    <!-- <link rel="stylesheet" href="../../../assets/css/bulma.min.css"> -->
+
+    <link rel="stylesheet" href="../../assets/css/comum.css">
     <link rel="stylesheet" href="../../../assets/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="../../assets/css/icofont.min.css">
+    <link rel="stylesheet" href="../../assets/css/login.css">
 
     <title>R-Ponto</title>
 </head>
 <body>
-    <!-- <div class="card">teste</div> -->
-    <div class="card">Teste</div>
+    <form class="form-login" action="#" method="POST">
+        <div class="login-card card">
+            <div class="card-header">
+                <i class="icofont-user-suited mr-2"></i>
+                <span class="font-weight-bold">R</span>
+                <span class="font-weight-light">-Ponto</span>
+            </div>
+            
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email">E-Mail</label>
+                    <input type="email" id="email" name="email"
+                        class="form-control" placeholder="Digite seu email"
+                        autofocus
+                    >
+                </div>
+                
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password"
+                        class="form-control" placeholder="Digite sua senha"
+                        
+                    >
+                </div>
+                
+            </div>
+
+            <div class="card-footer">
+                <button class="btn btn-lg btn-primary">Entrar</button>
+            </div>
+        </div>
+
+    </form>
 </body>
 
 </html>
