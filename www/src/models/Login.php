1f1009fef99921bc648bf26547aa672806193b3d
VashRogers <rogerssj2@gmail.com>
2022-10-18 16:21:49 -0400

    Retirando mais um var_dumo

--- www/src/models/Login.php
+++ www/src/models/Login.php
@@ -7,7 +7,6 @@ class Login extends Model{
     public function checkLogin() {
         $user = User::getOne(['email' => $this->email]);
         
-        var_dump($user);
         if($user){
             if(password_verify($this->password, $user->password)){
                 return $user;
